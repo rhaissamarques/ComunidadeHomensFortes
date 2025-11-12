cdeef330bdb6ec894652b5e87ebe3a5b39a352b9
Rhaissa Marques <rhaissa.marques@gmail.com>
2025-11-12 16:26:34 -0300

    changing path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
+      <Route path={"/ComunidadeHomensFortes/"} component={Home} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
