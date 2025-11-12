5fad4612eca2ef0418b9f8f01ccba8987a4a74f1
Rhaissa Marques <rhaissa.marques@gmail.com>
2025-11-12 17:00:26 -0300

    changing red color and logo image path

--- client/src/const.ts
+++ client/src/const.ts
@@ -2,7 +2,7 @@ export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
 
 export const APP_TITLE = import.meta.env.VITE_APP_TITLE || "App";
 
-export const APP_LOGO = "/logo1.png"; // Logo da Comunidade Homens Fortes
+export const APP_LOGO = "./public/logo1.png"; // Logo da Comunidade Homens Fortes
 
 // Generate login URL at runtime so redirect URI reflects the current origin.
 export const getLoginUrl = () => {
